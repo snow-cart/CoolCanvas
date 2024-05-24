da6352a7dc94745f4f06f397179e9d0211375cda
Mokocraft <kietas1237@gmail.com>
2024-05-24 17:04:39 +0300

    add screan clearing, change bg to black
    gaming

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var width, height;
+
 class Point {
 	constructor (x, y, deltaX, deltaY) {
 		this.x=x;
@@ -12,8 +14,8 @@ class Point {
 		let deltaX = this.deltaX;
 		let deltaY = this.deltaY;
 
-		if (x + deltaX > 1000) {
-			this.x = 2000 - x - deltaX;
+		if (x + deltaX > width) {
+			this.x = 2*width - x - deltaX;
 			this.deltaX = -deltaX;
 		}
 		else if (x + deltaX < 0) {
@@ -21,8 +23,8 @@ class Point {
 			this.deltaX = -deltaX;
 		} else this.x += deltaX;
 
-		if (y + deltaY > 1000) {
-			this.y = 2000 - y - deltaY;
+		if (y + deltaY > height) {
+			this.y = 2*height - y - deltaY;
 			this.deltaY = -deltaY;
 		}
 		else if (y + deltaY < 0) {
@@ -41,13 +43,17 @@ document.addEventListener("DOMContentLoaded", () =>{
 	var ctx = c.getContext("2d");
 	var cty = c.getContext("2d");
 
+	width = c.width;
+	height = c.height;
+
 	cty.rect(0, 0, 1000, 1000);
 	cty.fillStyle = "rgb(255, 255, 255, 5%)";
+	ctx.lineWidth = 5;
 
 	let pointArr = [];
 	let pointCount = 4;
 	for (let i=0; i<pointCount; i++) 
-		pointArr.push(new Point(f(1000), f(1000), f(10, 5), f(10, 5)));
+		pointArr.push(new Point(f(width), f(height), f(5, 3), f(5, 3)));
 
 	document.getElementById("myButton").addEventListener("click", () => {
 	for (let i=0; i<100; i++)
@@ -58,21 +64,24 @@ document.addEventListener("DOMContentLoaded", () =>{
 	drawLine(ctx, pointArr);
 	});
 
-	setInterval(() => {drawLine(ctx, pointArr)}, 100);
+	setInterval(() => {drawLine(ctx, pointArr)}, 10);
 
 });
 
 function drawLine (ctx, pointArr=[]) {
 	
-	ctx.strokeStyle = "rgb(0 0 255 / 10%)";
+	ctx.fillStyle = "rgb(0 0 0 / 20%)";
+	ctx.fillRect(0, 0, width, height);
+
+	ctx.strokeStyle = "rgb(3 252 219 / 100%)";
 	ctx.beginPath();
 	ctx.moveTo(pointArr[0].x, pointArr[0].y);
-	pointArr[0].move();
 	for (let i=1; i<pointArr.length; i++) {
 		ctx.lineTo(pointArr[i].x, pointArr[i].y);
 		pointArr[i].move();
 	}
 	ctx.lineTo(pointArr[0].x, pointArr[0].y);
+	pointArr[0].move();
 	ctx.stroke();
 	// console.log("AAAA");
 }
